4ddf5f8860f002785fe23ce97cf292e34de8100d
Joey Yip <Joey@knoesis.org>
2020-04-16 21:37:35 -0400

    Added countries & country latest data GraphQL API

--- server/models/schema.js
+++ server/models/schema.js
@@ -1,10 +1,12 @@
 const graphql = require('graphql');
 const axios = require('axios');
+const _ = require('lodash');
 
 const { GraphQLObjectType, 
         GraphQLString, 
         GraphQLInt,
         GraphQLSchema,
+        GraphQLList,
         GraphQLID } = graphql;
 
 const GlobalLatestDataType = new GraphQLObjectType({
@@ -18,20 +20,64 @@ const GlobalLatestDataType = new GraphQLObjectType({
     })
 })
 
+const CountryLatestDataType = new GraphQLObjectType({
+    name: "CountryLatestData", 
+    fields: () => ({
+        confirmed: { type: GraphQLInt },
+        confirmedToday: { type: GraphQLInt },
+        recovered: { type: GraphQLInt },
+        deaths: { type: GraphQLInt },
+        deathsToday: { type: GraphQLInt },
+        country: { type: GraphQLString },
+        countryCode: { type: GraphQLString },
+        confirmedByDay: { type: GraphQLList(GraphQLInt) },
+        recoveredByDay: { type: GraphQLList(GraphQLInt) },
+        lastUpdated: { type: GraphQLString },
+        active: { type: GraphQLInt },
+        critical: { type: GraphQLInt },
+        mortalityPer: { type: GraphQLString },
+        recoveredPer: { type: GraphQLString }
+    })
+})
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         GlobalLatestData: {
             type: GlobalLatestDataType,
             async resolve(parent, args) {
-                let data = await axios.get('http://covid.delalify.com/api/latest')
+                let data = await axios.get('https://covid.delalify.com/api/latest')
                 if (data.status == 200) {                
                     return data.data.response
                 } else {
                     return data.data.message
                 }
             }
+        },
+        CountriesLatestData: {
+            type: new GraphQLList(CountryLatestDataType),
+            async resolve(parent, args) {
+                let data = await axios.get('https://covid.delalify.com/api/countries')
+                if (data.status == 200) {
+                    return data.data.response
+                } else {
+                    return data.data.message
+                }
+            }
+        },
+        CountryLatestData: {
+            type: CountryLatestDataType,
+            args: {country: {type: GraphQLString}},
+            async resolve(parent, args) {
+                let data = await axios.get('https://covid.delalify.com/api/countries')
+                if (data.status == 200) {
+                    return _.find(data.data.response, {country: args.country})
+                } else {
+                    return data.data.message
+                }
+            }
         }
+        
     }
 })
 
