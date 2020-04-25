5cc4f460fafb4b8ee0da4306810a96c4b341f483
Joey Yip <Joey@knoesis.org>
2020-04-25 13:43:42 -0400

    Fixed graphql endpoint

--- server/app.js
+++ server/app.js
@@ -18,13 +18,24 @@ app.use(express.json());
 app.use(cors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    optionsSuccessStatus: 200 /* some legacy browsers (IE11, various SmartTVs) choke on 204 */,
+    optionsSuccessStatus: 200
 }))
 
 
-app.use('/graphql', cors(), graphqlHTTP({
-    schema
-    // graphiql: true
+app.use('/graphql', (req,res,next)=>{
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'content-type, authorization, content-length, x-requested-with, accept, origin');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    res.header('Allow', 'POST, GET, OPTIONS')
+    res.header('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  }, graphqlHTTP({
+    schema,
+    graphiql: true
 }))
 
 // Serve static assets if in production
