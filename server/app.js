608d04dd76c5690acca35c8e19f2c51027846854
Joey Yip <Joey@knoesis.org>
2020-04-25 11:59:04 -0400

    Fixed graphql endpoint

--- server/app.js
+++ server/app.js
@@ -15,9 +15,19 @@ const app = express();
 app.use(express.json());
 
 // Enable cors
-// app.use(cors());
+app.use(cors());
 
-app.use('/graphql', cors(), graphqlHTTP({
+app.use("/graphql", function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
+app.use('/graphql', graphqlHTTP({
     schema
     // graphiql: true
 }))
