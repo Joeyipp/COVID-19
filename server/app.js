d199e4441f12044ee64da9fe34b7a0a3ae2cc107
Joey Yip <Joey@knoesis.org>
2020-04-25 11:38:40 -0400

    Fixed graphql endpoint

--- server/app.js
+++ server/app.js
@@ -14,22 +14,6 @@ const app = express();
 // Body parser
 app.use(express.json());
 
-const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.sendStatus(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.use(allowCrossDomain);
-
 // Enable cors
 app.use(cors());
 
