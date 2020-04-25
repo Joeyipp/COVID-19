03ec0a67c3a16debf4f2d79f6cad80031ee8ecaa
Joey Yip <Joey@knoesis.org>
2020-04-25 14:00:38 -0400

    Fixed graphql endpoint

--- server/app.js
+++ server/app.js
@@ -3,8 +3,19 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const { ApolloServer } = require('apollo-server-express');
 
 const schema = require('./models/schema')
+
+const server = new ApolloServer({
+	schema,
+	playground: false,
+	context: ({req, res}) => {
+		return {
+			request: req
+		};
+	}
+});
 
 // load env vars
 dotenv.config({path: './config/config.env'});
@@ -16,27 +27,32 @@ app.use(express.json());
 
 // Enable cors
 app.use(cors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    optionsSuccessStatus: 200
-}))
+	origin: true,
+	credentials: true,
+}));
+
+server.applyMiddleware({
+	app,
+	path: '/graphql',
+	cors: false,
+});
 
 
-app.use('/graphql', (req,res,next)=>{
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Headers', 'content-type, authorization, content-length, x-requested-with, accept, origin');
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-    res.header('Allow', 'POST, GET, OPTIONS')
-    res.header('Access-Control-Allow-Origin', '*');
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-  }, graphqlHTTP({
-    schema,
-    graphiql: true
-}))
+// app.use('/graphql', (req,res,next)=>{
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Headers', 'content-type, authorization, content-length, x-requested-with, accept, origin');
+//     res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+//     res.header('Allow', 'POST, GET, OPTIONS')
+//     res.header('Access-Control-Allow-Origin', '*');
+//     if (req.method === 'OPTIONS') {
+//       res.sendStatus(200);
+//     } else {
+//       next();
+//     }
+//   }, graphqlHTTP({
+//     schema,
+//     graphiql: true
+// }))
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
